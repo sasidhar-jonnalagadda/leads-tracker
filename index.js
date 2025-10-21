28b1e9de61a172712e0c676ffb06e724424884f7
Sasidhar Jonnalagadda <sasidharj1234@gmail.com>
2025-10-21 23:25:59 +0530

    Update JS file
    Updated JS file to improve performance of the extension

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-let myLeads = ["www.lead1.com", "www.lead2.com", "www.lead3.com"];
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
+  inputEl.value = "";
+  renderLeads();
 });
 
-for (let i = 0; i < myLeads.length; i++) {
-  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+function renderLeads() {
+  let listItems = "";
+
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems +=
+      "<li><a href='" +
+      myLeads[i] +
+      "' target='_blank'>" +
+      myLeads[i] +
+      "</a></li>";
+  }
+
+  ulEl.innerHTML = listItems;
 }
