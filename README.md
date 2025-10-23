8a0603e424d3da2f89b1d26ea243081a4e9175ec
Sasidhar Jonnalagadda <sasidharj1234@gmail.com>
2025-10-23 22:21:35 +0530

    Add manifest.json and SAVE TAB button
    Added manifest.json file and SAVE TAB button to directly save the current URL

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# leads-tracker
+# Project Title
 
+Leads Tracker
+
+# Description
+
+A browser extension that helps businesses and individuals save URLs of potential customer profiles for later contact.
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
@@ -10,6 +11,14 @@ if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   render(myLeads);
 }
+
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
 
 deleteBtn.addEventListener("dblclick", function () {
   localStorage.clear();
