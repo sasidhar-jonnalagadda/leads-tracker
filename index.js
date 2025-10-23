d4b3503555b8e2ebad3e1e9847604bc46d56d5b6
Sasidhar Jonnalagadda <sasidharj1234@gmail.com>
2025-10-23 21:09:00 +0530

    Update index.js
    Modified renderLeads() to accept parameters

--- index.js
+++ index.js
@@ -8,27 +8,27 @@ let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
-  renderLeads();
+  render(myLeads);
 }
 
 deleteBtn.addEventListener("dblclick", function () {
   localStorage.clear();
   myLeads = [];
-  renderLeads();
+  render(myLeads);
 });
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderLeads();
+  render(myLeads);
 });
 
-function renderLeads() {
+function render(leads) {
   let listItems = "";
 
-  for (let i = 0; i < myLeads.length; i++) {
-    listItems += `<li><a href='${myLeads[i]}' target='_blank'>${myLeads[i]}</a></li>`;
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `<li><a href='${leads[i]}' target='_blank'>${leads[i]}</a></li>`;
   }
 
   ulEl.innerHTML = listItems;
